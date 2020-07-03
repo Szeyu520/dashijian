8a64b5409a649eb13fc37100dfc5ae3f96057442
kabuda <943019067@qq.com>
2020-07-03 22:42:12 +0800

    第三天

---
+++ routers/cate.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const path = require('path')
+const db = require(path.join(__dirname, '../common/db.js'))
+const router = express.Router()
+
+// 查询文章分类列表数据
+router.get('/cates', async (req, res) => {
+  // 直接操作数据库查询所有的文章分类列表数据
+  let sql = 'select * from category where is_delete = 0'
+  let ret = await db.operateData(sql)
+  if (ret && ret.length > 0) {
+    // 正常查询到数据
+    res.json({
+      status: 0,
+      message: '查询分类成功！',
+      data: ret
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '查询分类失败！'
+    })
+  }
+})
+
+// 添加分类接口
+router.post('/addcates', async (req, res) => {
+  // 获取请求参数
+  let param = req.body
+  // 操作数据库
+  let sql = 'insert into category set ?'
+  let ret = await db.operateData(sql, param)
+  // 响应请求
+  if (ret && ret.affectedRows > 0) {
+    res.json({
+      status: 0,
+      message: '添加分类成功！'
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '添加分类失败！'
+    })
+  }
+})
+
+
+
+router.get('/deletecate/:id', async (req, res) => {
+  let id = req.params.id
+  let sql = 'update category set is_delete = 1 where id = ?'
+  let ret = await db.operateData(sql, id)
+  if (ret && ret.affectedRows > 0) {
+    res.json({
+      status: 0,
+      message: '删除分类成功！'
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '删除分类失败！'
+    })
+  }
+})
+
+// 根据id查询分类的详情
+router.get('/cates/:id', async (req, res) => {
+  // 获取分类的id
+  let id = req.params.id
+  // 数据库操作
+  let sql = 'select * from category where id = ?'
+  let ret = await db.operateData(sql, id)
+  if (ret && ret.length > 0) {
+    res.json({
+      status: 0,
+      message: '查询分类信息成功！',
+      data: ret[0]
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '查询分类信息失败！'
+    })
+  }
+})
+
+// 更新分类
+router.post('/updatecate', async (req, res) => {
+  // 获取请求参数
+  let param = req.body
+  // 数据库操作
+  // SQL语句中不区分大小写，但是参数的名称区分大小写
+  let sql = 'update category set ? where Id = ?'
+  let ret = await db.operateData(sql, [{name: param.name, alias: param.alias}, param.Id])
+  // 响应请求
+  if (ret && ret.affectedRows > 0) {
+    res.json({
+      status: 0,
+      message: '更新分类成功！'
+    })
+  } else {
+    res.json({
+      status: 1,
+      message: '更新分类失败！'
+    })
+  }
+})
+
+module.exports = router
--- routers/user.js
+++ routers/user.js
@@ -1,40 +1,107 @@
+/*
+  用户信息相关接口
+*/
 const express = require('express')
 const router = express.Router()
+const utils = require('utility')
 const path = require('path')
 const db = require(path.join(__dirname, '../common/db.js'))
 
-router.get('/userinfo', async (req, res) => {
+// 路由配置
 
+// 获取用户信息
+router.get('/userinfo', async (req, res) => {
+    // 根据什么获取用户的详细信息？用户的id
+    // 用户的id从哪里得到？token
+    // req.user表示从token中获取的信息，该信息是登录成功后放入的
+    // req.user属性名称是固定的吗？是的
+    // { username: 'tom', id: 8, iat: 1593587638, exp: 1593591238 }
+    // 根据用户的id查询用户的详细信息
     let sql = 'select id, username, nickname, email, user_pic from user where id = ?'
-    let info = await db.aperateData(sql, req.user.id)
+    let info = await db.operateData(sql, req.user.id)
     if (info && info.length > 0) {
         res.json({
             status: 0,
-            message: '获取用户信息成功',
+            message: '获取用户基本信息成功！',
             data: info[0]
         })
     } else {
         res.json({
             status: 1,
-            message: '获取用户信息失败',
+            message: '获取用户信息失败！'
         })
     }
-
-
 })
 
-
-router.post('/userinfo', (req, res) => {
-    res.send('update userinfo')
+// 更新用户信息
+router.post('/userinfo', async (req, res) => {
+    // 获取请求参数
+    let param = req.body
+    // 更新用户的信息
+    let sql = 'update user set ? where id = ?'
+    // 如果是增删改操作，那么返回对象；如果是查询，那么返回数组
+    let ret = await db.operateData(sql, [{ nickname: param.nickname, email: param.email }, param.id])
+    // 处理响应状态
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: '修改用户信息成功！'
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '修改用户信息失败！'
+        })
+    }
 })
 
-router.post('/updatepwd', (req, res) => {
-    res.send('updatepwd')
+// 更新密码
+router.post('/updatepwd', async (req, res) => {
+    // 获取请求参数
+    let param = req.body
+    // 对密码进行加密处理
+    param.oldPwd = utils.md5(param.oldPwd)
+    param.newPwd = utils.md5(param.newPwd)
+    // 获取用户的id
+    let id = req.user.id
+    // 调用数据库方法进行更新操作
+    let sql = 'update user set password = ? where id = ? and password = ?'
+    let ret = await db.operateData(sql, [param.newPwd, id, param.oldPwd])
+    // 响应返回状态
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: '更新密码成功！'
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '更新密码失败！'
+        })
+    }
 })
 
-router.post('/update/avatar', (req, res) => {
-    res.send('update/avatar')
+// 更新头像
+router.post('/update/avatar', async (req, res) => {
+    // 获取请求参数
+    let param = req.body
+    // 获取用户的id
+    let id = req.user.id
+    // 更新数据库
+    let sql = 'update user set user_pic = ? where id = ?'
+    let ret = await db.operateData(sql, [param.avatar, id])
+    // 处理响应
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: '更新头像成功！'
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '更新头像失败！'
+        })
+    }
 })
-
 
 module.exports = router
